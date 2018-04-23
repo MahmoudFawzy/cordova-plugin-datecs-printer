82ca00067266671898e573bf7a41e41b0d12a181
mahmoud <dev@itworkco.com>
2018-04-23 19:24:31 +0200

    2

--- src/android/M3Printer.java
+++ src/android/M3Printer.java
@@ -5,9 +5,21 @@ import com.nbbse.mobiprint3.*;
 
 
 import org.apache.cordova.*;
-import org.json.JSONObject;
+import org.json.*; 
+import java.io.*; 
+import java.util.*;
+import android.graphics.*;
+import android.graphics.Bitmap.*;
+import android.util.Xml.*; 
+
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+
 
 public class M3Printer extends CordovaPlugin {
 	public static Printer print;
@@ -22,12 +34,26 @@ public class M3Printer extends CordovaPlugin {
 		if (action.equals("printText")) { 
             String txt = args.getString(0);  
 			print.printText(txt);
+			print.printEndLine();
 			callbackContext.success("1");
             return true; 
 		} 
 		else if (action.equals("printBase64")) { 
-            String txt = args.getString(0); 
-			print.printText(txt);
+			String txt = args.getString(0); 
+			 
+			final byte[] decodedBytes = Base64.decode(txt, Base64.DEFAULT);
+
+			Bitmap bitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+
+			 
+			int mWidth = bitmap.getWidth();
+			int mHeight = bitmap.getHeight();
+
+			bitmap = resizeImage(bitmap, 48 * 8, mHeight);
+			
+			printer.printBitmap(bitmap);
+
+			print.printEndLine();
 			callbackContext.success("1");
             return true; 
 		} 
